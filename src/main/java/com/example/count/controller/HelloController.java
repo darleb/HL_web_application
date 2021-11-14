7a6ca2caee6c1df6dd6523b4777df4a3614efb71
dasa <dar.lebedeva@mail.tu>
2021-11-14 13:30:36 +0500

    redis

--- src/main/java/com/example/count/controller/HelloController.java
+++ src/main/java/com/example/count/controller/HelloController.java
@@ -1,15 +1,19 @@
 package com.example.count.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
-    private int counter = 0;
+    @Autowired
+    private StringRedisTemplate template;
 
     @GetMapping
-    public int getCounter() {
-        counter++;
-        return counter;
+    public Long getCounter() {
+        ValueOperations<String, String> ops = template.opsForValue();
+        return ops.increment("count");
     }
 }
